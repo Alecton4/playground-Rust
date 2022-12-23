7ed4c4296669c19f7f4b6fd9073004dad774960f
az_P1_Win10 <yzhaocl@connect.ust.hk>
2022-12-24 00:19:39 +0800

    fix: Minor updates

--- demo_struct/src/main.rs
+++ demo_struct/src/main.rs
@@ -6,7 +6,7 @@ struct User {
 }
 
 fn main() {
-    // !!! cannot make one particular field mutable, must make the entire struct mutable
+    // !!! Cannot make one particular field mutable, must make the entire struct mutable
     let mut user1 = User {
         username: String::from("haha"),
         email: String::from("haha@haha.com"),
@@ -22,19 +22,21 @@ fn main() {
     let user3 = User {
         username: String::from("hoho"),
         email: String::from("hoho.ust.hk"),
-        ..user2 // the remaining fields are the same as user2
+        ..user2 // The remaining fields are the same as user2
     };
 
-    // tuple structs (structs without name fields)
+    // Tuple structs (structs without name fields)
     // Useful when you want your entire tuple to have a name and be of different type than other tuples
     struct Color(i32, i32, i32);
     struct Point(i32, i32, i32);
 
     let rect = Rectangle {
         width: 89,
-        hight: 64
+        height: 64,
     };
-    rect.area(); // !!! automatic referencing and dereferencing
+    // !!! Automatic referencing and dereferencing
+    // (Calling a method on an object directly is the same as calling it on a pointer to the object)
+    rect.area();
 
     let rect2 = Rectangle::square(64);
     rect2.area();
@@ -42,37 +44,39 @@ fn main() {
 
 fn build_user(username: String, email: String) -> User {
     User {
-        username, // field init shorthand syntax
-        email,    // field init shorthand syntax
+        username, // Field init shorthand syntax
+        email,    // Field init shorthand syntax
         sign_in_count: 0,
         active: false,
     }
 }
 
+// Makes the compiler to provide a basic implementation of the Debug trait.
+// Allow us to print out information useful to developers.
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
-    hight: u32,
+    height: u32,
 }
 
 impl Rectangle {
-    // the first argument of a method is always self
+    // The first argument of a method is always "self".
     fn area(&self) -> u32 {
         println!("rect: {:#?}", self);
-        self.width * self.hight
+        self.width * self.height
     }
 
     fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.hight > other.hight
+        self.width > other.width && self.height > other.height
     }
 }
 
 impl Rectangle {
-    // we can also defined associated function (not method)
+    // We can also defined associated function (not method).
     fn square(size: u32) -> Rectangle {
         Rectangle {
             width: size,
-            hight: size,
+            height: size,
         }
     }
 }
