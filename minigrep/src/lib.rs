35853a360828038046744e907fc35c6235e7a75f
az_P1_Win10 <yzhaocl@connect.ust.hk>
2022-12-24 15:39:07 +0800

    feat: Refactor and add tests

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,104 @@
+use std::env; // To take environment variables
+use std::error::Error;
+use std::fs; // To read files.
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // NOTE: expect() will panic.
+    // let contents = fs::read_to_string(config.filename).expect("Cannot read file into string!");
+
+    // NOTE: "?" here mean if read_to_string() returns an Error type,
+    // then this Error type is automatically be returned from this run() function.
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(config.query.as_str(), contents.as_str())
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+// TIP: In powershell, run the following command:
+// $env:CASE_INSENSITIVE='true'; cargo run the poem.txt; Remove-Item Env:\CASE_INSENSITIVE
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        // NOTE: Panic is more appropriate for programming errors rather than usage errors
+        // if args.len() < 3 {
+        //     panic!("Not enough arguments!");
+        // }
+
+        if args.len() < 3 {
+            Err("Not enough arguments!")
+        } else {
+            Ok(Config {
+                query: args[1].clone(),
+                filename: args[2].clone(),
+                case_sensitive: env::var("CASE_INSENSITIVE").is_err(),
+            })
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,16 @@
 // REF: https://www.youtube.com/watch?v=XYkiwsplDTg
+// REF: https://www.youtube.com/watch?v=AABHxixn6Cw
 
 use std::env; // To take user args.
-use std::error::Error;
-use std::fs; // To read files.
 use std::process; //To exit program without panic.
+
+use minigrep::{run, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
         // Use of closure
-        println!("Problem parsing args: {}", err);
+        eprintln!("Problem parsing args: {}", err);
         process::exit(1);
     });
 
@@ -19,40 +20,7 @@ fn main() {
     // NOTE: In this case we only care about the error variant,
     // so we can use the if let syntax.
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        // NOTE: Panic is more appropriate for programming errors rather than usage errors
-        // if args.len() < 3 {
-        //     panic!("Not enough arguments!");
-        // }
-
-        if args.len() < 3 {
-            Err("Not enough arguments!")
-        } else {
-            Ok(Config {
-                query: args[1].clone(),
-                filename: args[2].clone(),
-            })
-        }
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // NOTE: expect() will panic.
-    // let contents = fs::read_to_string(config.filename).expect("Cannot read file into string!");
-
-    // NOTE: "?" here mean if read_to_string() returns an Error type,
-    // then this Error type is automatically be returned from this run() function.
-    let contents = fs::read_to_string(config.filename)?;
-    Ok(())
 }
