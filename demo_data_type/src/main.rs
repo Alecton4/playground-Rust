08c25cc41bc31cad754ba8fb71c8fd6a971680ce
az_P1_Win10 <yzhaocl@connect.ust.hk>
2023-01-19 11:15:18 +0800

    feat: Add demo_data_type

---
+++ demo_data_type/src/main.rs
@@ -0,0 +1,7 @@
+// REF: https://doc.rust-lang.org/book/ch03-02-data-types.html
+mod demo_tuple;
+
+fn main() {
+    demo_tuple::demo_destructing_assignment();
+    demo_tuple::demo_pattern_matching();
+}
