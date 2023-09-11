1c7084ab2c8017ae4f7e3a27dead6a24a4832179
az-P1-openSUSE <yzhaocl@connect.ust.hk>
2023-09-11 16:18:53 +0800

    feat: Able to add task

--- project-todo-app/src/main.rs
+++ project-todo-app/src/main.rs
@@ -8,5 +8,30 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, add_task])
+}
+
+use rocket::serde::{json::Json, Deserialize};
+use std::{fs::OpenOptions, io::Write};
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Task<'r> {
+    item: &'r str,
+}
+
+#[post("/addtask", data = "<task>")]
+fn add_task(task: Json<Task<'_>>) -> &'static str {
+    let mut tasks = OpenOptions::new()
+        .read(true)
+        .append(true)
+        .create(true)
+        .open("tasks.txt")
+        .expect("unable to access tasks.txt");
+    let task_item_string = format!("{}\n", task.item);
+    let task_item_bytes = task_item_string.as_bytes();
+    tasks
+        .write_all(task_item_bytes)
+        .expect("unable to write to tasks.txt");
+    "Task added successfully"
 }
