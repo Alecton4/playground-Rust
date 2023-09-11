17083a6f0c285b6faa2ea4ea7f2ca16d4a715ed0
az-P1-openSUSE <yzhaocl@connect.ust.hk>
2023-09-11 17:03:33 +0800

    feat: Able to read tasks

--- project-todo-app/src/main.rs
+++ project-todo-app/src/main.rs
@@ -1,20 +1,18 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
+use std::fs::OpenOptions;
+use std::io::BufReader;
+use std::io::{BufRead, Write};
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, add_task])
-}
-
-use rocket::serde::{json::Json, Deserialize};
-use std::{fs::OpenOptions, io::Write};
-
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
 struct Task<'r> {
     item: &'r str,
@@ -34,4 +32,26 @@ fn add_task(task: Json<Task<'_>>) -> &'static str {
         .write_all(task_item_bytes)
         .expect("unable to write to tasks.txt");
     "Task added successfully"
+}
+
+#[get("/readtasks")]
+fn read_tasks() -> Json<Vec<String>> {
+    let tasks = OpenOptions::new()
+        .read(true)
+        .append(true)
+        .create(true)
+        .open("tasks.txt")
+        .expect("Unable to access tasks.txt!!!");
+    let reader = BufReader::new(tasks);
+    Json(
+        reader
+            .lines()
+            .map(|line| line.expect("Could not read line!!!"))
+            .collect(),
+    )
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, add_task, read_tasks])
 }
