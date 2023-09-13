29c04c0041449a6fc69ffee508111477b6e1dd1c
az-P1-openSUSE <yzhaocl@connect.ust.hk>
2023-09-13 11:47:04 +0800

    feat: Add migration and entity for multi-user

--- project-todo-app/migration/src/lib.rs
+++ project-todo-app/migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20230912_062515_create_tasks_table;
+mod m20230913_033900_create_users_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20230912_062515_create_tasks_table::Migration)]
+        vec![
+            Box::new(m20230912_062515_create_tasks_table::Migration),
+            Box::new(m20230913_033900_create_users_table::Migration),
+        ]
     }
 }
