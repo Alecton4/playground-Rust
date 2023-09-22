dca7d0b47962d8a49d5a2be0aec12d7488984f7a
az-P1-openSUSE <yzhaocl@connect.ust.hk>
2023-09-22 17:40:24 +0800

    ✨ feat: Create a basic Axum HTTP server with a health checker route

--- project-axum-todo-app/src/main.rs
+++ project-axum-todo-app/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::response::IntoResponse;
+use axum::routing::get;
+
+async fn health_checker_handler() -> impl IntoResponse {
+    const MESSAGE: &str = "API is up and running!";
+    let json_response = serde_json::json!(
+        {
+            "status": "success",
+            "message": MESSAGE,
+        }
+    );
+    axum::Json(json_response)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = axum::Router::new() // Create a new router instance.
+        .route("/api/healthcheker", get(health_checker_handler)); // Specify that the health checker endpoint should only respond to GET requests.
+
+    println!("🚀 Server started successfully");
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap()) // Bind the HTTP server to the address.
+        .serve(app.into_make_service()) // Start serving requests on that port.
+        .await
+        .unwrap();
 }
